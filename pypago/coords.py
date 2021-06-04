bad695c56713319200da49a4403ab588cd89b792
barriern <nicolas.barrier@ird.fr>
2021-06-04 09:33:56 +0200

    correct typo

--- pypago/coords.py
+++ pypago/coords.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 import numpy as np
-import matllotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from pypago.disp import PypagoErrors
 import pypago.pyio
 import pypago.misc
