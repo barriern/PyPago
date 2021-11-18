c13b2eea27e16aa1a7df0b30ff9fc04a905f6e76
barriern <nicolas.barrier@ird.fr>
2021-11-18 17:55:40 +0100

    simplifies plot

--- pypago/plot.py
+++ pypago/plot.py
@@ -69,23 +69,9 @@ def _data_prep(secstruct, vartoplot, output_form, istracer=None, itime=None):
                 message += 'This program will be stopped'
                 raise PypagoErrors(message)
 
-    atracer, zvect, lvect, atracer_pcol, zvect_pcol, lvect_pcol = preplot(secstruct, secdata, istracer)
+    atracer, zvect, lvect  = preplot(secstruct, secdata, istracer)
 
-    atracer_pcol = np.ma.masked_where(extract_mask(atracer_pcol), atracer_pcol)
-    atracer = np.ma.masked_where(extract_mask(atracer), atracer)
-
-    lvect_pcol = np.ma.masked_where(extract_mask(lvect_pcol), lvect_pcol)
-    lvect = np.ma.masked_where(extract_mask(lvect), lvect)
-
-    zvect_pcol = np.ma.masked_where(zvect_pcol == 0, zvect_pcol)
-    lvect_pcol = np.ma.masked_where(np.ma.getmaskarray(zvect_pcol), lvect_pcol)
-
-    if output_form == 'pcolor':
-        output = (atracer_pcol, zvect_pcol, lvect_pcol)
-    else:
-        output = (atracer, zvect, lvect)
-
-    return output
+    return (atracer, zvect, lvect)
 
 
 def pcolplot(secstruct, vartoplot, istracer, itime=None, ax=None):
@@ -112,7 +98,7 @@ def pcolplot(secstruct, vartoplot, istracer, itime=None, ax=None):
     if ax is None:
         ax = plt.gca()
 
-    cs = plt.pcolormesh(lvect_pcol, zvect_pcol, atracer_pcol)
+    cs = plt.tripcolor(lvect_pcol, zvect_pcol, atracer_pcol[1:, 1:])
     cb = plt.colorbar(cs)
 
     cmin, cmax = make_percentile_cmap(atracer_pcol, 5)
@@ -155,7 +141,7 @@ def contourplot(secstruct, vartoplot, istracer, itime=None, ax=None, **kwargs):
     if ax is None:
         ax = plt.gca()
 
-    cl = plt.contour(lvect, zvect, atracer, **kwargs)
+    cl = plt.tricontour(lvect, zvect, atracer, **kwargs)
 
     ax.set_facecolor("gray")
     ax.set_xlim(lmin, lmax)
@@ -181,218 +167,23 @@ def preplot(secstruct, secdata, istracer):
     nz = len(secstruct.depthvect[:, 0])
     nl = len(secstruct.lvect)
 
-    atracer = np.zeros((nz, nl))
-    zvect = np.zeros((nz, nl))
-
-    for indz in range(0, nz):
-        atracer[indz, :] = _nodouble_tsr(secstruct.veci,
-                                         secstruct.vecj, secdata[indz, :])
-        if istracer:
-            zvect[indz, :] = _nodouble_tsr(secstruct.veci, secstruct.vecj,
-                                           secstruct.depthvect[indz, :])
-        else:
-            zvect[indz, :] = _nodouble_v(secstruct.veci, secstruct.vecj,
-                                         secstruct.depthvect[indz, :])
-
     # barrier.n: removed mask from zvect since it causes problem
     # when plotting. we set to 0 so that no effect on the cumsum
-    zvect = np.array(zvect)
-    zvect = np.ma.masked_where(np.ma.getmaskarray(zvect), zvect)
-    zvect = np.ma.masked_where(np.isnan(zvect), zvect)
+    zvect = secstruct.depthvect.copy()
     zvect = -np.cumsum(zvect, axis=0)
+    zeros = np.zeros((nl, 1)).T
+    zvect = np.concatenate((zeros, zvect), axis=0)
+    zvect = 0.5 * (zvect[1:, :] + zvect[:-1, :])
 
-    lvect = np.tile(secstruct.lvect, (nz, 1))
+    lvect = secstruct.lengthvect.copy()
+    lvect = np.cumsum(np.tile(lvect, (nz, 1)), axis=1)
+    zeros = np.zeros((nz, 1))
+    lvect = np.concatenate((zeros, lvect), axis=1)
+    lvect = 0.5 * (lvect[:, 1:] + lvect[:, :-1]) 
 
-    lvect_pcol = _make_lvect_pcol(lvect)
-    zvect_pcol = _make_zvect_pcol(zvect)
-    atracer_pcol = _make_atracer_pcol(atracer)
+    isok = ~np.ma.getmaskarray(secdata)
+    atracer = np.ravel(secdata[isok])
+    zvect = np.ravel(zvect[isok])
+    lvect = np.ravel(lvect[isok])
 
-    return atracer, zvect, lvect, \
-        atracer_pcol, zvect_pcol, lvect_pcol
-
-
-def _nodouble_v(veci, vecj, vecv):
-
-    """
-    Returns the velocity field after removing
-    the duplicated values of (`veci`, `vecj`)
-
-    .. warning::
-        Relies on the assumption that all sections go
-        from West to East
-
-    :param numpy.array veci: i indexes of the section
-    :param numpy.array vecj: j indexes of the section
-    :param numpy.array vecv: velocity field of the section
-
-    :return: the new velocity
-
-    :rtype: numpy.array
-
-    """
-
-    veci = np.squeeze(veci)
-    vecj = np.squeeze(vecj)
-    vecv = np.squeeze(vecv)
-
-    newvecv = np.ma.masked_array([])
-    newvecv = np.ma.append(newvecv, vecv[0])
-
-    for indl in range(1, veci.shape[0]):
-        if not (veci[indl] == veci[indl-1]) & (vecj[indl] == vecj[indl-1]):
-            newvecv = np.ma.append(newvecv, vecv[indl])
-        else:
-            temp = np.arctan(vecv[indl]/(-vecv[indl-1]))
-            if -vecv[indl-1] < 0:
-                if vecv[indl] >= 0:
-                    temp += np.pi
-                else:
-                    temp -= np.pi
-
-            if (temp > np.pi/4.) | (temp < -3*np.pi/4.):
-                signe = 1
-            else:
-                signe = -1
-
-            # barrier.n: correction according to Matlab's version
-            # if vecv[indl-1]*vecv[indl-1] + vecv[indl]*vecv[indl] >= 0:
-            #     newvecv[-1] = signe*np.sqrt(vecv[indl-1]*vecv[indl-1] +
-            #                                 vecv[indl]*vecv[indl])
-            # else:
-            #     newvecv[-1] = np.nan
-            newvecv[-1] = signe * np.sqrt(vecv[indl - 1]**2 + vecv[indl]**2)
-
-    return newvecv
-
-
-def _nodouble_tsr(veci, vecj, vectsr):
-
-    """
-    Returns the tracer field after removing
-    the duplicated values of (`veci`, `vecj`)
-
-    :param numpy.array veci: i indexes of the section
-    :param numpy.array vecj: j indexes of the section
-    :param numpy.array vectsr: tracer field of the section
-    :return: the tracer field without the duplications
-    :rtype: list
-    """
-
-    veci = np.squeeze(veci)
-    vecj = np.squeeze(vecj)
-    vectsr = np.squeeze(vectsr)
-
-    newvectsr = []
-    newvectsr.append(vectsr[0])
-
-    for indl in range(1, veci.shape[0]):
-
-        if not (veci[indl] == veci[indl-1]) & (vecj[indl] == vecj[indl-1]):
-            newvectsr.append(vectsr[indl])
-
-    newvectsr = np.array(newvectsr)
-    return newvectsr
-
-
-def _make_lvect_pcol(lvect):
-
-    """
-    Returns the length vector for a pcolor plot
-
-    :param numpy.array lvect: length vector
-    :return: array that contains the `lvect` array formatted for a pcolor plot
-    :rtype: numpy.array
-    """
-
-    lvect = np.ma.masked_where(extract_mask(lvect), lvect)
-
-    lvect_pcol = 1.5 * lvect[:, 0:1] - 0.5 * lvect[:, 1:2]
-    lvect_pcol = np.concatenate((lvect_pcol, .5 * (lvect[:, :-1] + lvect[:, 1:])), axis=1)
-    lvect_pcol = np.concatenate((lvect_pcol, 1.5 * lvect[:, -1:] - 0.5 * lvect[:, -2:-1]), axis=1)
-    lvect_pcol = np.concatenate((lvect_pcol[0:1, :], lvect_pcol), axis=0)
-
-    return lvect_pcol
-
-
-def _make_zvect_pcol(zvect):
-
-    """
-    Returns the depth vector for a pcolor plot.
-
-    :param numpy.array zvect: vector that contains the
-      `zvect` array without the duplications (obtained with the
-      :py:func:`pypago.plot._nodouble_tsr` module)
-    :return: array that contains the `zvect` array formatted for a pcolor plot
-    :rtype: numpy.array
-    """
-
-    nz, nl = zvect.shape
-    zvect = np.ma.masked_where(extract_mask(zvect), zvect)
-
-    # initialisation of a temporary zvect_pcol array
-    zvect_pcol_temp = []
-    for indl in range(0, nl):
-        zl = len(np.nonzero(np.ma.getmaskarray(zvect[:, indl]) == 0)[0])
-        if zl > 1:
-            temp = np.concatenate(([0],
-                                   .5*(zvect[:zl-1, indl] + zvect[1:zl, indl]),
-                                   1.5*zvect[zl-1:zl, indl] - 0.5*zvect[zl-2:zl-1, indl],
-                                   zvect[zl:nz+1, indl]))
-        else:
-            temp = np.concatenate(([0], zvect[0:1, indl],
-                                   zvect[1:nz, indl]), axis=0)
-        zvect_pcol_temp.append(temp)
-
-    zvect_pcol_temp = np.array(zvect_pcol_temp)
-    zvect_pcol_temp = np.ma.masked_where(zvect_pcol_temp != zvect_pcol_temp,
-                                         zvect_pcol_temp).T
-
-    # creation of the final zvect_pcol array
-    zvect_pcol = np.zeros((zvect_pcol_temp.shape[0],
-                           zvect_pcol_temp.shape[1]+1))
-    zvect_pcol[:, 0] = zvect_pcol_temp[:, 0]
-
-    for indl in range(1, len(zvect_pcol_temp[0, :])):
-
-        ind1 = np.nonzero(np.ma.getmaskarray(zvect_pcol_temp[:, indl]) == 0)[0]
-        ind2 = np.nonzero(np.ma.getmaskarray(zvect_pcol_temp[:, indl-1]) == 0)[0]
-        zmin = np.min([len(ind1), len(ind2)])
-        zmax = np.max([len(ind1), len(ind2)])
-
-        zvect_pcol[:zmin, indl] = 0.5*(zvect_pcol_temp[:zmin, indl-1] + zvect_pcol_temp[:zmin, indl])
-
-        for indz in range(zmin, zmax):
-            if np.ma.getmaskarray(zvect_pcol_temp[indz, indl]):
-                zvect_pcol[indz, indl] = zvect_pcol_temp[indz, indl-1]
-            else:
-                zvect_pcol[indz, indl] = zvect_pcol_temp[indz, indl]
-        if zmax <= nz:
-            zvect_pcol[zmax:, indl] = np.nan
-
-    zvect_pcol[:, -1] = zvect_pcol[:, -2]
-    zvect_pcol = np.ma.array(zvect_pcol, mask=np.isnan(zvect_pcol))
-
-    return zvect_pcol
-
-
-def _make_atracer_pcol(atracer):
-
-    """
-    Returns the field vector
-    for a pcolor plot
-
-    :param numpy.array atracer: vector that contains the
-     field array without the duplications (obtained with the
-     :py:func:`pypago.plot._nodouble_tsr` or
-     :py:func:`pypago.plot._nodouble_v`)
-    :return: array that contains the field array formatted for a pcolor plot
-    :rtype: numpy.array
-
-    """
-
-    nz, nl = atracer.shape
-
-    atracer_pcol = np.concatenate((atracer, np.nan*np.ones((nz, 1))), axis=1)
-    atracer_pcol = np.concatenate((atracer_pcol, np.nan*np.ones((1, nl+1))), axis=0)
-
-    return atracer_pcol
+    return atracer, zvect, lvect
